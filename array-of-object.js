7e75235536cee2e2aa01edf4604fce9ff7814f93
yohanessahrul <yohanessahrul92@gmail.com>
2018-03-07 11:48:19 +0700

    Release 1

--- array-of-object.js
+++ array-of-object.js
@@ -14,4 +14,18 @@ createObj('Adhit', 2, 'Male')
 createObj('Tama', 2, 'Male')
 createObj('Rifki', 3, 'Male')
 
+console.log('Release 0')
 console.log(arr)
+
+
+console.log('Release 1')
+
+function getData(name){
+    for(let i=0; i<arr.length; i++){
+        if(arr[i].name == name){
+            return arr[i];
+        }
+    }
+}
+
+console.log(getData('Icha'))
