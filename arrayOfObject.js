b468332fb514211fd1d5c0c761971a1d09ac7c1e
annisaayu <annisaayu569@gmail.com>
2018-03-07 22:12:05 +0700

    create arrayOfObject.js

---
+++ arrayOfObject.js
@@ -0,0 +1,23 @@
+var arr=[];
+function createObj(name, phase, gender) {
+  let obj = {};
+  obj.name = name;
+  obj.phase = phase;
+  obj.gender = gender;
+  arr.push(obj);
+}
+createObj('Akbar', 1, 'male')
+createObj('Icha', 1, 'female')
+createObj('Adhit', 2, 'male')
+createObj('Tama', 2, 'male')
+createObj('Rifky', 2, 'male')
+
+// console.log(arr);
+function getData(name){
+  for(let i=0; i<arr.length; i++){
+    if(name === arr[i].name){
+      return arr[i]
+    }
+  }
+}
+console.log(getData('Icha'));
